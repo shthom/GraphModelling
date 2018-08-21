e0eb5a5576537951e37369d5072d922d5a6b0961
Shibu Thomas <shibut@microsoft.com>
2018-08-21 15:50:13 +0530

    Add project files.

---
+++ GraphModelling/LoadGraph.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GraphModelling
+{
+    class LoadGraph
+    {
+        public LoadGraph(Graph<int> g , string filename)
+        {
+
+            FileStream fs = new FileStream(filename, FileMode.Open,FileAccess.Read);
+            using (var reader = new StreamReader(fs, Encoding.UTF8))
+            {
+
+
+                string line;
+
+                while ( (line = reader.ReadLine()) != null){
+                    string[] values = line.Split(' ');
+                    GraphNode<int> nodeFrom = new GraphNode<int>(int.Parse(values[0]));
+                    GraphNode<int> nodeTo = new GraphNode<int>(int.Parse(values[1]));
+                    double distance = double.Parse(values[2]);
+                    GraphEdge<int> edge = new GraphEdge<int>(nodeFrom, nodeTo, distance);
+                    g.AddVertex(nodeFrom);
+                    g.AddVertex(nodeTo);
+                    g.AddEdge(nodeFrom, nodeTo, distance);
+
+
+
+                }
+            } 
+        }
+
+    }
+}
---
+++ GraphModelling/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphModelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //BellmanFordTestMethod();
+            //FloydWarshallFunction();
+            JohnsonFunction();
+
+           
+
+           
+
+
+
+            Console.ReadLine();
+
+        }
+
+        private static void JohnsonFunction()
+        {
+            Graph<int> g = new Graph<int>();
+            LoadGraph loader = new LoadGraph(g, @"data\GraphWithNoNegativeCycle.txt");
+            var alg = new GraphTraversal();
+            var result = alg.JohnsonFunction(g);
+        }
+
+        private static void FloydWarshallFunction()
+        {
+            Graph<int> g = new Graph<int>();
+            LoadGraph loader = new LoadGraph(g, @"data\SimpleGraphWithNegativeWeights.txt");
+            GraphNode<int> start = g.GetVertex(new GraphNode<int>(0));
+            var alg = new GraphTraversal();
+            var result = alg.FloydWarshallFunction(g);
+
+            if( result != null)
+            {
+                foreach(var item in result)
+                {
+                    Tuple<GraphNode<int>, GraphNode<int>> keyset = item.Key;
+                    GraphNode<int> nodeFrom = keyset.Item1;
+                    GraphNode<int> nodeTo = keyset.Item2;
+                    Console.WriteLine("Shortest distance from Node: " + nodeFrom.Value + " To: " + nodeTo.Value + " = " + item.Value);
+
+                }
+                
+            }
+            Console.WriteLine("Path found: ");
+        }
+
+        static void BellmanFordTestMethod()
+        {
+            Graph<int> g = new Graph<int>();
+            LoadGraph loader = new LoadGraph(g, @"data\SimpleGraphWithNegativeWeights.txt");
+            GraphNode<int> start = g.GetVertex(new GraphNode<int>(0));
+            var alg = new GraphTraversal();
+            var result = alg.BellmanFordFunction(g, start);
+            Console.WriteLine("Negative Cycle found: " + result.Item1);
+
+
+            
+        }
+    }
+}
